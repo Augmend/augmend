15d3c18465688a4a3d56daf60b6a4ee978770621
Raaghavi Sivaguru <rasivagu@microsoft.com>
2020-07-28 14:16:02 -0700

    Code scan

--- repo_scan.rb
+++ repo_scan.rb
@@ -19,59 +19,93 @@ class CodeScan
     def initialize(installation_client, repository)
         @installation_client = installation_client
         @repository = repository
+        @base_branch = 'main'
+        @new_branch = 'augmend-bot-scan'
+        @repo_updated = false
     end
 
+    # start() - main method called by the server
+    #   1. creates a dummy branch to check-in any code changes
+    #   2. scan the all the files in the repo iteratively & update the branch with code changes (if any)
+    #   3. if the code changes have been made, create a pull request or delete the created dummy branch
     def start
         create_branch
-        contents = @installation_client.contents(@repository)
+        process(nil)
+        if @repo_updated
+            create_pull_request
+        else
+            delete_branch
+        end
+    end
+
+    def process(content)
+        contents = nil
+        if content == nil
+            contents = @installation_client.contents(@repository)
+        else
+            contents = @installation_client.contents(@repository, :path => content.path)
+        end
         contents.each do |content|
             if content.type === 'file'
                 puts "Processing file #{content.name}"
                 process_file(content)
             elsif content.type === 'dir'
                 puts "Processing directory #{content.name}"
-                process_directories(content)
+                process(content)
             end
         end
     end
 
-    def process_file(content)
-        URI.open(content.download_url) {|f|
-          line_number = 0
-          lines = []
-          f.each_line do |line|
-            line_number += 1
-            fixed_line = line
-            REPLACEMENT_WORDS.keys.each do |word|
-              if fixed_line.downcase.include?(word)
-                fixed_line = fixed_line.gsub(word, REPLACEMENT_WORDS[word])
-              end
-            end
-            lines.push(fixed_line)
-          end
-
-          @installation_client.update_contents(
-              @repository,
-              content.path,
-              "Updating content",
-              content.sha,
-              lines.join(),
-              :branch => "augmend-repo-scan")
-        }
-    end
-
-    def process_directories(content)
-    end
-
     def create_branch
-        master = @installation_client.refs(@repository, "heads/master")
-        base_branch_sha = master.object.sha
+        base_branch = @installation_client.refs(@repository, "heads/#{@base_branch}")
+        sha = base_branch.object.sha
         
         puts "Creating a branch.."
-        @installation_client.create_ref(@repository, "heads/augmend-repo-scan", base_branch_sha)
+        @installation_client.create_ref(@repository, "heads/#{@new_branch}", sha)
     end
 
     def delete_branch
         puts "Deleting the branch.."
+        @installation_client.delete_branch(@repository, @new_branch)
+    end
+
+    def create_pull_request
+        puts "Creating a pull request"
+        @installation_client.create_pull_request(
+            @repository,
+            @base_branch,
+            @new_branch,
+            "Augmend bot replaced racially insensitive words in the repo",
+            "Augmend bot replaced racially insensitive words in the repo")
+    end
+
+    # Scan a file to replace inappropriate words
+    # Update the repo with the altered file
+    def process_file(content)
+        file_changed = false
+        URI.open(content.download_url) {|f|
+            lines_to_be_written = []
+            f.each_line do |original_line|
+                changed_line = original_line
+                REPLACEMENT_WORDS.keys.each do |word|
+                    if changed_line.downcase.include?(word)
+                        file_changed = true
+                        changed_line = changed_line.gsub(word, REPLACEMENT_WORDS[word])
+                    end
+                end
+                lines_to_be_written.push(changed_line)
+            end
+
+            if file_changed
+                @repo_updated = true
+                @installation_client.update_contents(
+                    @repository,
+                    content.path,
+                    "Augmend bot updated file content",
+                    content.sha,
+                    lines_to_be_written.join(),
+                    :branch => @new_branch)
+            end
+        }
     end
 end
