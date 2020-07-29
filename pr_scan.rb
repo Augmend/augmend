115d37027d5ee293232828a966be501d887a537c
Harshitha Akkaraju <hakkaraj@microsoft.com>
2020-07-29 11:51:23 -0700

    Added code to add interactive comments seperately

--- pr_scan.rb
+++ pr_scan.rb
@@ -7,6 +7,18 @@ class PRScan
     @installation_client = installation_client
   end
 
+  def get_variable_names_with_block_words(line)
+    tokenized = line.split(" ")
+    terms_with_block_words = []
+    tokenized.each do |token|
+      REGEX.keys.each do |regex_key| 
+        match = Regexp.new(regex_key, "i").match(token)
+        terms_with_block_words.push(token) unless match.nil?
+      end
+    end
+    return terms_with_block_words
+  end
+
   def handle_new_pull_request(payload)
     repo = payload['repository']['full_name']
     pr_number = payload['pull_request']['number']
@@ -19,30 +31,65 @@ class PRScan
       file_name = item.filename # path
       comments_array = []
 
+      # create a variable occurence map to keep track of multiple usages of the same block word
+      variable_occurences_map = {}
       URI.open(file_raw_url) {|f|
         line_number = 0
         f.each_line do |line|
           line_number += 1
+
           fixed_line = replace_block_words(line)
           next if fixed_line == line
 
-          # TODO: update body text
+          variable_names = get_variable_names_with_block_words(line)
+          variable_names.each do |var_name|
+            occurences = variable_occurences_map[var_name]
+            if occurences.nil?
+              occurences = []
+            end
+
+            occurences.push({
+              :line_number => line_number,
+              :line_contents => fixed_line
+            })
+            variable_occurences_map[var_name] = occurences
+          end
+        end
+      }
+
+      puts "variable map: #{variable_occurences_map}"
+
+      # depending on the number of occurences of the variable, add a regular or an interactive comment to the PR
+      variable_occurences_map.keys.each do |var_name|
+        puts occurences
+        occurences = variable_occurences_map[var_name]
+        if occurences.length() > 2
+          first_occurence = occurences[0]
+          body_text = "Augmend bot discovered multiple usages of this term. Reply `Yes` to this comment if you'd like us to make the changes everywhere else."
           comments_array += [{
             :path => file_name,
-            :line => line_number,
-            :body => "test this text\n```suggestion\n#{fixed_line.strip}\n```",
+            :line => first_occurence.line_number,
+            :body => "#{body_text}\n```suggestion\n#{first_occurence.fixed_line.strip}\n```",
           }]
+        else
+           body_text = "Augmend bot recommends the following changes:"
+           occurences.each do |occurence|
+            comments_array += [{
+              :path => file_name,
+              :line => occurence.line_number,
+              :body => "#{body_text}\n```suggestion\n#{occurence.fixed_line.strip}\n```",
+            }]
+           end
         end
+      end
 
-        options = {
-          :body => "overall body",
-          :commit_id => sha,
-          :event => "REQUEST_CHANGES",
-          :comments => comments_array
-        }
-
-        @installation_client.post("/repos/#{repo}/pulls/#{pr_number}/reviews", options)
+      options = {
+        :body => "Augmend bot scan results: \n",
+        :commit_id => sha,
+        :event => "REQUEST_CHANGES",
+        :comments => comments_array
       }
+      @installation_client.post("/repos/#{repo}/pulls/#{pr_number}/reviews", options)
     end
   end
 end
